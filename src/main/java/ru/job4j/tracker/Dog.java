59e7131be2d5f0509c198daf9470b71d026fe6d2
Gukovdaniel <gukov.danila@bk.ru>
2022-01-17 04:10:31 +0300

    Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/tracker/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class Dog {
+    public static void main(String[] args) {
+       Dog polcan = new Dog();
+       Dog zhuchka = new Dog();
+       Dog sharik = new Dog();
+    }
+}

18a465f6bda514d8b6b7e8f86a30d945e8509e20
Gukovdaniel <gukov.danila@bk.ru>
2022-01-28 12:18:21 +0300

    Поля объекта [#166083 #252775]

--- src/main/java/ru/job4j/tracker/Cat.java
+++ src/main/java/ru/job4j/tracker/Cat.java
@@ -5,10 +5,6 @@ public class Cat {
     private String food;
     private String name;
 
-    public void giveNick(String nick) {
-        this.name = nick;
-    }
-
     public void show() {
         System.out.println(this.name);
         System.out.println(this.food);
@@ -17,15 +13,21 @@ public class Cat {
     public void eat(String meat) {
         this.food = meat;
     }
-
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
     public static void main(String[] args) {
         System.out.println("There are gav's food.");
         Cat gav = new Cat();
         gav.eat("kotleta");
+        gav.giveNick("Гав");
         gav.show();
         System.out.println("There are black's food.");
         Cat black = new Cat();
         black.eat("fish");
+        black.giveNick("Черный");
         black.show();
+
     }
+
 }

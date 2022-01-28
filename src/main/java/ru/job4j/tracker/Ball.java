eb8cbf6550b44e7782163f4272b807d0a70b0f1a
Gukovdaniel <gukov.danila@bk.ru>
2022-01-28 09:20:47 +0300

    Вызов метода с возвращаемым типом. [#167387 #252770]

--- src/main/java/ru/job4j/tracker/Ball.java
+++ src/main/java/ru/job4j/tracker/Ball.java
@@ -2,9 +2,6 @@ package ru.job4j.tracker;
 
 public class Ball {
     public void tryRun(Hare condition) {
-        if (condition) {
-            System.out.println("Колобок съеден");
-        } else System.out.println("Колобок сбежал");
     }
 }
 
--- src/main/java/ru/job4j/tracker/DimmiDic.java
+++ src/main/java/ru/job4j/tracker/DimmiDic.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 public class DimmiDic {
 
     public String engToRus(String eng) {
+
         return "Неизвестное слово" + eng;
     }
 }

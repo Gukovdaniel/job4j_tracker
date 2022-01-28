3cbfc3cfc61d4b48f1ccea34b37264ac4dc1f618
Gukovdaniel <gukov.danila@bk.ru>
2022-01-28 09:13:39 +0300

    Вызов метода с возвращаемым типом. [#167387 #252770]

--- src/main/java/ru/job4j/tracker/DimmiDic.java
+++ src/main/java/ru/job4j/tracker/DimmiDic.java
@@ -3,8 +3,7 @@ package ru.job4j.tracker;
 public class DimmiDic {
 
     public String engToRus(String eng) {
-        String word = "Неизвестное слово";
-        return word + eng;
+        return "Неизвестное слово" + eng;
     }
 }
 

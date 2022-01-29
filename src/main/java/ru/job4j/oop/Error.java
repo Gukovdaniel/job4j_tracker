9277f491b3293ab51e44b08e0dc8a602e7bf9127
Gukovdaniel <gukov.danila@bk.ru>
2022-01-29 14:08:43 +0300

    Конструктор [#173889]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -5,23 +5,22 @@ public class Error {
         Error error2 = new Error();
         error2.printInfo();
     }
+
     boolean active;
     int status;
     String message;
-    public Error(){
-
+    public Error() {
     }
+
     public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
     }
+
     public void printInfo() {
         System.out.println("Активность " + active);
         System.out.println("Статус " + status);
         System.out.println("Сообщение " + message);
-
     }
-
-
 }

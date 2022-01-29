a8b7f33af7dc3d37135b503b2200e6de949834b8
Gukovdaniel <gukov.danila@bk.ru>
2022-01-29 13:30:04 +0300

    Взаимодействие объектов [#166087 #252887]

--- src/main/java/ru/job4j/tracker/Ball.java
+++ src/main/java/ru/job4j/tracker/Ball.java
@@ -1,10 +1,11 @@
 package ru.job4j.tracker;
 
 public class Ball {
-    public void tryRun(boolean condition){
-        if (condition){
+    public void tryRun(boolean condition) {
+        if (condition) {
             System.out.println("Колобок съеден");
-        }else {
+        }
+        else {
             System.out.println("Колобок сбежал");
         }
     }

a8a38cf98de52cc219536b78dcaac67a83eba1b8
Gukovdaniel <gukov.danila@bk.ru>
2022-02-08 12:45:36 +0300

    Зона видимости переменных [#180147 #261909]

--- src/main/java/ru/job4j/oop/Reduce.java
+++ src/main/java/ru/job4j/oop/Reduce.java
@@ -3,8 +3,8 @@ package ru.job4j.oop;
 public class Reduce {
     private int[] array;
 
-    public void to(int[] array2) {
-        array = array2;
+    public void to(int[] array) {
+        this.array = array;
     }
 
     public void print() {

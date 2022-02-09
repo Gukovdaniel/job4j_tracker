66e840e8851884f7f9540d2c52cb772064b301dc
Gukovdaniel <gukov.danila@bk.ru>
2022-02-09 19:29:10 +0300

    Переопределение [#174890 #262365]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,7 +2,6 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
-
     public String gen(String name, String body) {
         return "{" + System.lineSeparator()
                 + "\t\"name\" : \"" + name + "\"," + System.lineSeparator()

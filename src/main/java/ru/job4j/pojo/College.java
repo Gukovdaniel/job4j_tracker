3b76dbf4d1b63a34c51db0038e819cebdd3cf143
Gukovdaniel <gukov.danila@bk.ru>
2022-02-15 19:21:55 +0300

    Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Иванов Иван Иванович; ");
+        student.setNumber(3);
+        student.setCreated(new Date());
+
+        System.out.println(student.getFio() + student.getNumber() + " group; " + student.getCreated() + "admitted to study");
+    }
+}

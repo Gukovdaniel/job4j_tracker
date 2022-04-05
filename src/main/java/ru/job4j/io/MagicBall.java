c335ff9ad2b68964cca3df36ecf5bf5cd4cfddce
gukov <gukov.danila@bk.ru>
2022-04-05 21:07:07 +0300

    Чтение из консоли. Класс Scanner. [#179514]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -1,12 +1,24 @@
 package ru.job4j.io;
 
 import java.util.Scanner;
+import java.util.Random;
 
     public class MagicBall {
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
+            Scanner q = new Scanner(System.in);
             System.out.println("Hello, whats your name?");
             String name = input.nextLine();
-            System.out.println(name + ", Glad to see you!");
+            System.out.println(name + ", What do you want to know?");
+            String qu = q.nextLine();
+            int ans = new Random().nextInt(3);
+            if(ans == 0) {
+                System.out.println("Yes!");
+            } else if (ans == 1) {
+                System.out.println("No...");
+            } else {
+                System.out.println("Maybe)");
+            }
+
         }
     }

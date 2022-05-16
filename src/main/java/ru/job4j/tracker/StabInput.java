add9e1d3581c308af1bc77d3225fc385a39d8c55
gukov <gukov.danila@bk.ru>
2022-05-16 13:17:15 +0300

    Input и полиморфизм. [#181094]

--- src/main/java/ru/job4j/tracker/StabInput.java
+++ src/main/java/ru/job4j/tracker/StabInput.java
@@ -1,8 +1,9 @@
 package ru.job4j.tracker;
 
-public class StabInput implements Input{
+public class StabInput implements Input {
     private String[] answers;
     private int position = 0;
+
     public StabInput(String[] answers) {
         this.answers = answers;
     }

78949167b6bcdd38e862f0935e2d30def5fea8eb
gukov <gukov.danila@bk.ru>
2022-05-17 14:00:20 +0300

    Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -22,8 +22,7 @@ public class StartUITest {
         Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
-                "edited item"
+                String.valueOf(item.getId()),
         };
         StartUI.editItem(new StabInput(answers), tracker);
         Item edited = tracker.findById(item.getId());

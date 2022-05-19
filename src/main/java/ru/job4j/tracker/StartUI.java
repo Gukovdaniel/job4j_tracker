8c771faa5fba3dfcac573c452e1dc590f4607133
gukov <gukov.danila@bk.ru>
2022-05-19 08:31:02 +0300

    Реализация меню за счет шаблона стратегия. [#181780 #302280]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,52 +2,6 @@ package ru.job4j.tracker;
 
 public class StartUI {
 
-    public static void editItem(Input input, Tracker tracker) {
-        System.out.println("=== Edit the Item ===");
-        int id = input.askInt("Enter id: ");
-        String name = input.askStr("Enter new name item ");
-        Item item = new Item(name);
-        if (tracker.replace(id, item)) {
-            System.out.println("Заявка изменена успешно!");
-        } else {
-            System.out.println("Ошибка замены заявки");
-        }
-    }
-
-    public static void deleteItem(Input input, Tracker tracker) {
-        System.out.println("=== Delete the Item ===");
-        int id = input.askInt("Enter id: ");
-        if (tracker.delete(id)) {
-            System.out.println("Заявка удалена успешно!");
-        } else {
-            System.out.println("Ошибка удаления заявки!!!");
-        }
-    }
-
-    public static void findByName(Input input, Tracker tracker) {
-        System.out.println("=== Find items by name ===");
-        String name = input.askStr("Enter name: ");
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Заявки с именем " + name + " не найдены.");
-        }
-    }
-
-    public static void findById(Input input, Tracker tracker) {
-        System.out.println("=== Find item by id ===");
-        int id = input.askInt("Enter id: ");
-        Item item = tracker.findById(id);
-        if (item != null) {
-            System.out.println(item);
-        } else {
-            System.out.println("Заявка с введенным id: " + id + " не найдена");
-        }
-    }
-
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
@@ -73,8 +27,14 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction()
+                new CreateAction(),
+                new DeleteAction(),
+                new EditAction(),
+                new FindByNameAction(),
+                new FindByIdAction(),
+                new ShowAllAction()
         };
+
         new StartUI().init(input, tracker, actions);
     }
 }

02e11d733a41d66f0035174e6468545697bd236b
gukov <gukov.danila@bk.ru>
2022-05-22 13:19:25 +0300

    Тестирование исключений с junit [#219366 #303111]

--- src/test/java/ru/job4j/exseptions/FactTest.java
+++ src/test/java/ru/job4j/exseptions/FactTest.java
@@ -2,14 +2,10 @@ package ru.job4j.exseptions;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 public class FactTest {
 
-        @Test
+        @Test(expected = IllegalArgumentException.class)
         public void whenFactorialLessZero() {
-            int rsl = Fact.calc(3);
-            assertThat(rsl, is(6));
+            Fact.calc(-3);
         }
-    }
+}

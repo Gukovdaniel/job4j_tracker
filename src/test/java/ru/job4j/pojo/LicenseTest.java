5fa265113dc47333d666a6b40b10016e1e287f4f
gukov <gukov.danila@bk.ru>
2022-05-22 14:29:21 +0300

    Тестирование исключений с junit [#219366 #303111]

--- src/test/java/ru/job4j/pojo/LicenseTest.java
+++ src/test/java/ru/job4j/pojo/LicenseTest.java
@@ -9,7 +9,7 @@ public class LicenseTest {
     @Test
     public void eqName() {
         License first = new License();
-        first.setCode("");
+        first.setCode("audio");
         License second = new License();
         second.setCode("audio");
         assertThat(first, is(second));

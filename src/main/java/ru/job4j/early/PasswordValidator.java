f145b8c4b4cc4273813469201666aa168f62dc7d
gukov <gukov.danila@bk.ru>
2022-11-08 17:47:03 +0300

    Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -9,6 +9,30 @@ public class PasswordValidator {
         || password.equalsIgnoreCase("admin") || password.equalsIgnoreCase("user")) {
             System.out.println("Password shouldnt contain substrings: qwerty, 12345, admin, user");
         }
+        if (password.length() > 8 && password.length() < 32) {
+            System.out.println("Password should be length [8,32]");
+        }
+
+        char[] array = password.toCharArray();
+        for (char x : array) {
+            if (Character.isUpperCase(x)) {
+                System.out.println("Password should contain at least one uppercase letter");
+            }
+        }
+
+        char[] array2 = password.toCharArray();
+        for (char x : array2) {
+            if (Character.isLowerCase(x)) {
+                System.out.println("Password should contain at least one lowercase letter");
+            }
+        }
         return password;
     }
 }
+
+//boolean result=false;
+//        char[] array=str.toCharArray();
+//        for(char x:array){
+//            if(Character.isUpperCase(x)){
+//                result=true;
+//                break;

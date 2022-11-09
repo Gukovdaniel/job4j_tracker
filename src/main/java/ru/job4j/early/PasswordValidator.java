3bf76bf540f4974e966631d441658108ad62d1ff
gukov <gukov.danila@bk.ru>
2022-11-09 22:54:58 +0300

    Принципы раннего возврата и охранных выражений [#504875 #359019]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -2,30 +2,67 @@ package ru.job4j.early;
 
 public class PasswordValidator {
     public static String validate(String password) {
-        if (password.equals(null)) {
+        if (password.equalsIgnoreCase(null)) {
             throw new IllegalArgumentException("Password cant be null!!!");
         }
-        if (password.equalsIgnoreCase("qwerty") || password.equalsIgnoreCase("12345")
-        || password.equalsIgnoreCase("admin") || password.equalsIgnoreCase("user")) {
-            System.out.println("Password shouldnt contain substrings: qwerty, 12345, admin, user");
+        String[] ignore = new String[] {"qwerty", "12345", "admin", "user" };
+        for (String s : ignore) {
+            if (s.equalsIgnoreCase(password)) {
+                System.out.println("Password should not contain substrings: qwerty, 12345, admin, user");
+            }
         }
-        if (password.length() > 8 && password.length() < 32) {
+
+        if (password.length() < 8 && password.length() > 32) {
             System.out.println("Password should be length [8,32]");
         }
 
+        boolean up = false;
         char[] array = password.toCharArray();
         for (char x : array) {
             if (Character.isUpperCase(x)) {
-                System.out.println("Password should contain at least one uppercase letter");
+                up = true;
+            } else {
+                continue;
             }
         }
-
+        boolean lo = false;
         char[] array2 = password.toCharArray();
         for (char x : array2) {
             if (Character.isLowerCase(x)) {
-                System.out.println("Password should contain at least one lowercase letter");
+                lo = true;
+            } else {
+                continue;
             }
         }
-        return password;
+        boolean nm = false;
+        char[] array3 = password.toCharArray();
+        for (char x : array3) {
+            if (Character.isDigit(x)) {
+                nm = true;
+            } else {
+                continue;
+            }
+        }
+        boolean iso = false;
+        char[] array4 = password.toCharArray();
+        for (char x : array4) {
+            if (Character.isISOControl(x)) {
+                iso = true;
+            } else {
+                continue;
+            }
+        if (!up) {
+            System.out.println("Password should contain at least one uppercase letter");
+        }
+        if (!lo) {
+                System.out.println("Password should contain at least one lowercase letter");
+            }
+        if (!nm) {
+                System.out.println("Password should contain at least one figure");
+            }
+        if (!iso) {
+                System.out.println("Password should contain at least one symbol");
+            }
+        } return password;
     }
 }

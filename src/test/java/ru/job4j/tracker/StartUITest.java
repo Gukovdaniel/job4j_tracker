888c9a4c11fb0289df54774c6eff5d4597eea861
gukov <gukov.danila@bk.ru>
2022-11-09 08:38:57 +0300

    Тесты вывода на консоль [#33585 #335480]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -128,7 +128,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
         Input in = new StabInput(
-                new String[]{"0", String.valueOf(one.getId()), "5"}
+                new String[]{"0", "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new ShowAllAction(out),
@@ -154,7 +154,7 @@ public class StartUITest {
             Tracker tracker = new Tracker();
             Item one = tracker.add(new Item("test1"));
             Input in = new StabInput(
-                    new String[] {"0", String.valueOf(one.getName()), "1"}
+                    new String[] {"0", one.getName(), "1"}
             );
             UserAction[] actions = new UserAction[]{
                     new FindByNameAction(out),

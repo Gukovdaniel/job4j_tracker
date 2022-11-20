30fc756a0304048a3f27c6410392911db7d6b07d
gukov <gukov.danila@bk.ru>
2022-11-20 21:15:25 +0300

    Map, HashMap [#212652]

--- src/main/java/ru/job4j/collections/UsageMap.java
+++ src/main/java/ru/job4j/collections/UsageMap.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("gukov.danya92@gmail.com" , "Гуков Даниил Васильевич");
+        map.put("gukov.danya92@gmail.com", "Гуков Даниил Васильевич");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " - " + value);

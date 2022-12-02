bd7078777f23b7ac00e0e5c8282003bea7eadfcc
gukov <gukov.danila@bk.ru>
2022-12-02 11:18:46 +0300

    Аттестация [#504878 #366642]

---
+++ src/main/java/ru/job4j/map/AnalyzeByMap.java
@@ -0,0 +1,61 @@
+package ru.job4j.map;
+
+import java.util.*;
+
+public class AnalyzeByMap {
+    public static double averageScore(List<Pupil> pupils) {
+        double result = 0;
+        int count = 0;
+        for (Pupil p : pupils) {
+            for (Subject n : p.subjects()) {
+                result += n.score();
+                count++;
+            }
+        }
+        return result / count;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        List listA = new ArrayList();
+        for (Pupil p : pupils) {
+            double result = 0;
+            int count = 0;
+            for (Subject n : p.subjects()) {
+                result += n.score();
+                count++;
+            }
+            Label label = new Label(p.name(), result / count);
+            listA.add(label);
+        }
+        return listA;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        List listA = new ArrayList();
+        double result = 0;
+        int count = 1;
+        for (Pupil p : pupils) {
+            for (Subject n : p.subjects()) {
+                result += n.score();
+                if (count < pupils.size()) {
+                    count++;
+                    break;
+                } else {
+                    Label label = new Label(n.name(), result / pupils.size());
+                    listA.add(label);
+                    result = 0;
+                    count = 1;
+                }
+            }
+        }
+        return listA;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        return null;
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        return null;
+    }
+}

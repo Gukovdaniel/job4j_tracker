996bd1ab2ccd10d5bbc2aed41314185d4096b91d
gukov <gukov.danila@bk.ru>
2022-12-04 10:55:04 +0300

    Организовать сортировку User [#10034 #367023]

--- src/main/java/ru/job4j/collections/User.java
+++ src/main/java/ru/job4j/collections/User.java
@@ -17,10 +17,8 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-      if (!this.name.equals(this.name)) {
-          return this.name.compareTo(o.name);
-      }
-        return Integer.compare(this.age, o.age);
+        int result = this.name.compareTo(o.name);
+        return result != 0 ? result : Integer.compare(this.age, o.age);
     }
 
     @Override

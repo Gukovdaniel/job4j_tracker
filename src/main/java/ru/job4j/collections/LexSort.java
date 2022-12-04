39a23d5d9620075d813903978c362e93be520508
gukov <gukov.danila@bk.ru>
2022-12-04 14:34:29 +0300

    Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collections/LexSort.java
@@ -0,0 +1,17 @@
+package ru.job4j.collections;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] numbAndStringLeft = left.split(".");
+        String leftNumber = numbAndStringLeft[0];
+        String[] numbAndStringRight = right.split(".");
+        String rightNumber = numbAndStringRight[0];
+        int l = Integer.parseInt(leftNumber);
+        int r = Integer.parseInt(rightNumber);
+        return Integer.compare(l,r);
+    }
+}

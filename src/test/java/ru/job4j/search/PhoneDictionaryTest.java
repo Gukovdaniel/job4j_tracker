385fc713ce67b205d1109fa11d231df8e920bda4
gukov <gukov.danila@bk.ru>
2022-12-13 11:29:36 +0300

    Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -16,14 +16,4 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
-
-    @Test
-    public void whenNotFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
-        phones.add(
-                new Person("Daniil", "Gukov", "654321", "St-Petersburg")
-        );
-        ArrayList<Person> persons = phones.find("Den");
-        assertThat(persons.isEmpty());
-    }
 }
